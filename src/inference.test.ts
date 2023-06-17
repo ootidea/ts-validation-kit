7f165be608ecdcdf61fab3c1e6e4c67df0d9f3e8
ootidea <ootidea@gmail.com>
2023-06-18 08:01:16 +0900

    refactor: grouping test cases

--- src/inference.test.ts
+++ src/inference.test.ts
@@ -1,95 +1,105 @@
 import { NonEmptyArray } from 'base-up'
-import { expectTypeOf } from 'vitest'
+import { describe, expectTypeOf, test } from 'vitest'
 import { z } from './index'
 
-test('Infer', () => {
+describe('Infer', () => {
   /**
-   * Helper function to infer the type of a schema.
+   * Helper function to infer the type from the schema.
    * It is convenient when used in combination with expectTypeOf.
    */
   function infer<const T>(value: T): z.Infer<T> {
     return value as any
   }
 
-  expectTypeOf(infer(z.boolean)).toEqualTypeOf<boolean>()
-  expectTypeOf(infer(z.number)).toEqualTypeOf<number>()
-  expectTypeOf(infer(z.bigint)).toEqualTypeOf<bigint>()
-  expectTypeOf(infer(z.string)).toEqualTypeOf<string>()
-  expectTypeOf(infer(z.symbol)).toEqualTypeOf<symbol>()
-  expectTypeOf(infer(z.null)).toEqualTypeOf<null>()
-  expectTypeOf(infer(z.undefined)).toEqualTypeOf<undefined>()
-  expectTypeOf(infer(z.nullish)).toEqualTypeOf<null | undefined>()
-  expectTypeOf(infer(z.void)).toEqualTypeOf<void>()
-  expectTypeOf(infer(z.unknown)).toEqualTypeOf<unknown>()
-  expectTypeOf(infer(z.any)).toEqualTypeOf<any>()
-  expectTypeOf(infer(z.never)).toEqualTypeOf<never>()
-
-  expectTypeOf(infer(z.literal('abc'))).toMatchTypeOf<'abc'>()
-  expectTypeOf(infer(z.literal(123))).toEqualTypeOf<123>()
-  expectTypeOf(infer(z.literal([]))).toEqualTypeOf<readonly []>()
-  expectTypeOf(infer(z.literal([1, 2, 3]))).toEqualTypeOf<readonly [1, 2, 3]>()
-  expectTypeOf(infer(z.literal({}))).toEqualTypeOf<{}>()
-  expectTypeOf(infer(z.literal({ name: 'Bob' }))).toEqualTypeOf<{ readonly name: 'Bob' }>()
-
-  expectTypeOf(infer(z.Array(z.boolean))).toEqualTypeOf<boolean[]>()
-  expectTypeOf(infer(z.Array(z.union(z.number, z.string)))).toEqualTypeOf<(number | string)[]>()
-  expectTypeOf(infer(z.NonEmptyArray(z.any))).toEqualTypeOf<NonEmptyArray<any>>()
-
-  expectTypeOf(infer(z.object({ name: z.string }))).toEqualTypeOf<{ name: string }>()
-  expectTypeOf(infer(z.object({ name: z.string }, { age: z.number }))).toEqualTypeOf<{
-    name: string
-    age?: number
-  }>()
-  expectTypeOf(infer(z.object({}))).toEqualTypeOf<{}>()
-
-  expectTypeOf(infer(z.union(z.number, z.undefined))).toEqualTypeOf<number | undefined>()
-  expectTypeOf(infer(z.union(z.literal('abc'), z.literal(123)))).toEqualTypeOf<'abc' | 123>()
-  expectTypeOf(infer(z.union())).toEqualTypeOf<never>()
-
-  expectTypeOf(infer(z.intersection(z.string, z.object({})))).toEqualTypeOf<string & {}>()
-  expectTypeOf(infer(z.intersection(z.object({ name: z.string }), z.object({ age: z.number })))).toEqualTypeOf<
-    {
+  test('atomic types', () => {
+    expectTypeOf(infer(z.boolean)).toEqualTypeOf<boolean>()
+    expectTypeOf(infer(z.number)).toEqualTypeOf<number>()
+    expectTypeOf(infer(z.bigint)).toEqualTypeOf<bigint>()
+    expectTypeOf(infer(z.string)).toEqualTypeOf<string>()
+    expectTypeOf(infer(z.symbol)).toEqualTypeOf<symbol>()
+    expectTypeOf(infer(z.null)).toEqualTypeOf<null>()
+    expectTypeOf(infer(z.undefined)).toEqualTypeOf<undefined>()
+    expectTypeOf(infer(z.nullish)).toEqualTypeOf<null | undefined>()
+    expectTypeOf(infer(z.void)).toEqualTypeOf<void>()
+    expectTypeOf(infer(z.unknown)).toEqualTypeOf<unknown>()
+    expectTypeOf(infer(z.any)).toEqualTypeOf<any>()
+    expectTypeOf(infer(z.never)).toEqualTypeOf<never>()
+  })
+  test('literal types', () => {
+    expectTypeOf(infer(z.literal('abc'))).toMatchTypeOf<'abc'>()
+    expectTypeOf(infer(z.literal(123))).toEqualTypeOf<123>()
+    expectTypeOf(infer(z.literal([]))).toEqualTypeOf<readonly []>()
+    expectTypeOf(infer(z.literal([1, 2, 3]))).toEqualTypeOf<readonly [1, 2, 3]>()
+    expectTypeOf(infer(z.literal({}))).toEqualTypeOf<{}>()
+    expectTypeOf(infer(z.literal({ name: 'Bob' }))).toEqualTypeOf<{ readonly name: 'Bob' }>()
+  })
+  test('Array types', () => {
+    expectTypeOf(infer(z.Array(z.boolean))).toEqualTypeOf<boolean[]>()
+    expectTypeOf(infer(z.Array(z.union(z.number, z.string)))).toEqualTypeOf<(number | string)[]>()
+    expectTypeOf(infer(z.NonEmptyArray(z.any))).toEqualTypeOf<NonEmptyArray<any>>()
+  })
+  test('object types', () => {
+    expectTypeOf(infer(z.object({ name: z.string }))).toEqualTypeOf<{ name: string }>()
+    expectTypeOf(infer(z.object({ name: z.string }, { age: z.number }))).toEqualTypeOf<{
       name: string
-    } & {
-      age: number
-    }
-  >()
-
-  expectTypeOf(infer(z.tuple(z.number, z.string))).toEqualTypeOf<[number, string]>()
-  expectTypeOf(infer(z.tuple())).toEqualTypeOf<[]>()
-
-  expectTypeOf(infer(z.literalUnion('abc', 123))).toEqualTypeOf<'abc' | 123>()
-  expectTypeOf(infer(z.literalUnion('abc'))).toEqualTypeOf<'abc'>()
-  expectTypeOf(infer(z.literalUnion())).toEqualTypeOf<never>()
-
-  expectTypeOf(infer(z.Record(z.string, z.number))).toEqualTypeOf<Record<string, number>>()
-  expectTypeOf(infer(z.Record(z.literal('a'), z.number))).toEqualTypeOf<{ a: number }>()
-  expectTypeOf(infer(z.Record(z.number, z.number))).toEqualTypeOf<Record<number, number>>()
-  expectTypeOf(infer(z.Record(z.literal(0), z.boolean))).toEqualTypeOf<{ 0: boolean }>()
-  expectTypeOf(infer(z.Record(z.symbol, z.any))).toEqualTypeOf<Record<symbol, any>>()
-  expectTypeOf(infer(z.Record(z.never, z.any))).toEqualTypeOf<Record<never, any>>()
-  expectTypeOf(infer(z.Record(z.boolean, z.any))).toEqualTypeOf<never>()
-
-  type List = { type: 'Nil' } | { type: 'Cons'; value: number; next: List }
-  expectTypeOf(
-    infer(
-      z.union(
-        z.object({ type: z.literal('Nil') }),
-        z.object({ type: z.literal('Cons'), value: z.number, next: z.recursion })
+      age?: number
+    }>()
+    expectTypeOf(infer(z.object({}))).toEqualTypeOf<{}>()
+  })
+  test('union types', () => {
+    expectTypeOf(infer(z.union(z.number, z.undefined))).toEqualTypeOf<number | undefined>()
+    expectTypeOf(infer(z.union(z.literal('abc'), z.literal(123)))).toEqualTypeOf<'abc' | 123>()
+    expectTypeOf(infer(z.union())).toEqualTypeOf<never>()
+  })
+  test('intersection types', () => {
+    expectTypeOf(infer(z.intersection(z.string, z.object({})))).toEqualTypeOf<string & {}>()
+    expectTypeOf(infer(z.intersection(z.object({ name: z.string }), z.object({ age: z.number })))).toEqualTypeOf<
+      {
+        name: string
+      } & {
+        age: number
+      }
+    >()
+  })
+  test('tuple types', () => {
+    expectTypeOf(infer(z.tuple(z.number, z.string))).toEqualTypeOf<[number, string]>()
+    expectTypeOf(infer(z.tuple())).toEqualTypeOf<[]>()
+  })
+  test('literalUnion shorthand', () => {
+    expectTypeOf(infer(z.literalUnion('abc', 123))).toEqualTypeOf<'abc' | 123>()
+    expectTypeOf(infer(z.literalUnion('abc'))).toEqualTypeOf<'abc'>()
+    expectTypeOf(infer(z.literalUnion())).toEqualTypeOf<never>()
+  })
+  test('Record types', () => {
+    expectTypeOf(infer(z.Record(z.string, z.number))).toEqualTypeOf<Record<string, number>>()
+    expectTypeOf(infer(z.Record(z.literal('a'), z.number))).toEqualTypeOf<{ a: number }>()
+    expectTypeOf(infer(z.Record(z.number, z.number))).toEqualTypeOf<Record<number, number>>()
+    expectTypeOf(infer(z.Record(z.literal(0), z.boolean))).toEqualTypeOf<{ 0: boolean }>()
+    expectTypeOf(infer(z.Record(z.symbol, z.any))).toEqualTypeOf<Record<symbol, any>>()
+    expectTypeOf(infer(z.Record(z.never, z.any))).toEqualTypeOf<Record<never, any>>()
+    expectTypeOf(infer(z.Record(z.boolean, z.any))).toEqualTypeOf<never>()
+  })
+  test('recursive types', () => {
+    type List = { type: 'Nil' } | { type: 'Cons'; value: number; next: List }
+    expectTypeOf(
+      infer(
+        z.union(
+          z.object({ type: z.literal('Nil') }),
+          z.object({ type: z.literal('Cons'), value: z.number, next: z.recursion })
+        )
       )
-    )
-  ).toEqualTypeOf<List>()
-
-  expectTypeOf(
-    infer(
-      z.Array(
-        z.recursive(
-          z.union(
-            z.object({ type: z.literal('Nil') }),
-            z.object({ type: z.literal('Cons'), value: z.number, next: z.recursion })
+    ).toEqualTypeOf<List>()
+    expectTypeOf(
+      infer(
+        z.Array(
+          z.recursive(
+            z.union(
+              z.object({ type: z.literal('Nil') }),
+              z.object({ type: z.literal('Cons'), value: z.number, next: z.recursion })
+            )
           )
         )
       )
-    )
-  ).toEqualTypeOf<List[]>()
+    ).toEqualTypeOf<List[]>()
+  })
 })
--- src/validation.test.ts
+++ src/validation.test.ts
@@ -2,70 +2,70 @@ import { describe, expect } from 'vitest'
 import { z } from './index'
 
 describe('isValid', () => {
-  it('boolean', () => {
+  test('boolean', () => {
     expect(z.isValid(true, z.boolean)).toBe(true)
     expect(z.isValid('true', z.boolean)).toBe(false)
   })
-  it('number', () => {
+  test('number', () => {
     expect(z.isValid(123, z.number)).toBe(true)
     expect(z.isValid('123', z.number)).toBe(false)
   })
-  it('bigint', () => {
+  test('bigint', () => {
     expect(z.isValid(123n, z.bigint)).toBe(true)
     expect(z.isValid(123, z.bigint)).toBe(false)
   })
-  it('string', () => {
+  test('string', () => {
     expect(z.isValid('abc', z.string)).toBe(true)
     expect(z.isValid(123, z.string)).toBe(false)
   })
-  it('symbol', () => {
+  test('symbol', () => {
     expect(z.isValid(Symbol(), z.symbol)).toBe(true)
     expect(z.isValid('abc', z.symbol)).toBe(false)
   })
-  it('null', () => {
+  test('null', () => {
     expect(z.isValid(null, z.null)).toBe(true)
     expect(z.isValid(undefined, z.null)).toBe(false)
   })
-  it('undefined', () => {
+  test('undefined', () => {
     expect(z.isValid(undefined, z.undefined)).toBe(true)
     expect(z.isValid(null, z.undefined)).toBe(false)
   })
-  it('nullish', () => {
+  test('nullish', () => {
     expect(z.isValid(null, z.nullish)).toBe(true)
     expect(z.isValid(undefined, z.nullish)).toBe(true)
     expect(z.isValid(0, z.nullish)).toBe(false)
   })
-  it('void', () => {
+  test('void', () => {
     expect(z.isValid(undefined, z.void)).toBe(true)
     expect(z.isValid(null, z.void)).toBe(false)
   })
-  it('unknown', () => {
+  test('unknown', () => {
     expect(z.isValid(123, z.unknown)).toBe(true)
   })
-  it('any', () => {
+  test('any', () => {
     expect(z.isValid(123, z.any)).toBe(true)
   })
-  it('never', () => {
+  test('never', () => {
     expect(z.isValid(123, z.never)).toBe(false)
   })
-  it('literal', () => {
+  test('literal', () => {
     expect(z.isValid('abc', z.literal('abc'))).toBe(true)
     expect(z.isValid('xyz', z.literal('abc'))).toBe(false)
     expect(z.isValid(123, z.literal('abc'))).toBe(false)
   })
-  it('Array', () => {
+  test('Array', () => {
     expect(z.isValid([false, true], z.Array(z.boolean))).toBe(true)
     expect(z.isValid([], z.Array(z.number))).toBe(true)
     expect(z.isValid({ 0: false, 1: true }, z.Array(z.boolean))).toBe(false)
     expect(z.isValid([123], z.Array(z.boolean))).toBe(false)
   })
-  it('NonEmptyArray', () => {
+  test('NonEmptyArray', () => {
     expect(z.isValid([false, true], z.NonEmptyArray(z.boolean))).toBe(true)
     expect(z.isValid([], z.NonEmptyArray(z.number))).toBe(false)
     expect(z.isValid({ 0: false, 1: true }, z.NonEmptyArray(z.boolean))).toBe(false)
     expect(z.isValid([123], z.NonEmptyArray(z.boolean))).toBe(false)
   })
-  it('object', () => {
+  test('object', () => {
     expect(z.isValid({ name: 'John' }, z.object({ name: z.string }))).toBe(true)
     expect(z.isValid({ name: 'John' }, z.object({ name: z.symbol }))).toBe(false)
 
@@ -73,20 +73,20 @@ describe('isValid', () => {
     expect(z.isValid({ name: 'John', age: 42 }, z.object({}, { age: z.number }))).toBe(true)
     expect(z.isValid({ name: 'John', age: '42' }, z.object({}, { age: z.number }))).toBe(false)
   })
-  it('union', () => {
+  test('union', () => {
     expect(z.isValid(123, z.union(z.boolean, z.number))).toBe(true)
     expect(z.isValid(123, z.union(z.literal(123), z.literal(456)))).toBe(true)
     expect(z.isValid(true, z.union())).toBe(false)
   })
-  it('intersection', () => {})
-  it('tuple', () => {
+  test('intersection', () => {})
+  test('tuple', () => {
     expect(z.isValid([true, 123], z.tuple(z.boolean, z.number))).toBe(true)
     expect(z.isValid([true, 123], z.tuple(z.boolean, z.number, z.string))).toBe(false)
     expect(z.isValid([true, 123], z.tuple(z.boolean))).toBe(false)
     expect(z.isValid([true, 123], z.tuple(z.string, z.bigint))).toBe(false)
     expect(z.isValid([], z.tuple())).toBe(true)
   })
-  it('Record', () => {
+  test('Record', () => {
     expect(z.isValid({ a: 1, b: 2 }, z.Record(z.string, z.number))).toBe(true)
     expect(z.isValid({ a: 1, b: 2 }, z.Record(z.number, z.number))).toBe(false)
 
@@ -96,7 +96,7 @@ describe('isValid', () => {
     expect(z.isValid({ a: true, 0: 'first' }, z.Record(z.string, z.any))).toBe(true)
     expect(z.isValid({ a: true, 0: 'first' }, z.Record(z.number, z.any))).toBe(false)
   })
-  it('recursion', () => {
+  test('recursion', () => {
     const listSchema = z.recursive(
       z.union(
         z.object({ type: z.literal('Nil') }),
