a1de5b209d8756058ad260308fa05264f3a689f7
ootidea <ootidea@gmail.com>
2024-10-01 20:51:41 +0900

    delete old source codes

--- src/validate.ts
+++
@@ -1,6 +0,0 @@
-import type { Infer } from './Infer'
-import type { SchemaBase } from './Schema'
-
-export function isValid<T extends SchemaBase>(schema: T, value: unknown): value is Infer<T> {
-  return schema.isValid(value)
-}
