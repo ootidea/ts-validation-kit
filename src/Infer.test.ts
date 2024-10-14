75a9e23427079071cd95f737e715063dbdb86584
ootidea <ootidea@gmail.com>
2024-10-14 16:55:45 +0900

    test: improve pipe infer test

--- src/Infer.test.ts
+++ src/Infer.test.ts
@@ -38,16 +38,17 @@ describe('Infer', () => {
   it('infers piped types', () => {
     expectInferredType(
       z.pipe(
-        z.bigint,
-        z.convert((value) => Number(value)),
+        z.number,
+        z.predicate((value) => value === 0 || value === 1),
+        z.predicate((value: 0 | 1) => Boolean(value)),
       ),
-    ).toBe<number>()
+    ).toBe<0 | 1>()
     expectInferredType(
       z.pipe(
-        z.string,
-        z.predicate((value) => value === '' || value === 'a'),
+        z.number,
+        z.convert(String),
         z.predicate((value) => value.length < 5),
       ),
-    ).toBe<'' | 'a'>()
+    ).toBe<string>()
   })
 })
